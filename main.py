f92f9296cc5616d14d7d1177d253324ad0a4a0d6
HankDeTank05 <henryhankholman@gmail.com>
2020-05-23 19:54:52 -0500

    collision code is WIP for p1, none for p2 yet

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+import random
+
 import arcade
 import worldgrid
 
-GRID_SIZE = 27
-
 TILE_SIZE = 32
-TILE_SPACE = 2
 
 WALL_WIDTH = 32
 WALL_DEPTH = 8
@@ -13,7 +12,14 @@ SCREEN_BORDER = 20
 
 SCREEN_SCALE = 1
 
-SCREEN_WIDTH = SCREEN_HEIGHT = GRID_SIZE * TILE_SIZE
+WIDTH = TILE_SIZE
+HEIGHT = TILE_SIZE
+MARGIN = WALL_DEPTH
+COLUMN_COUNT = 17
+ROW_COUNT = 17
+
+SCREEN_WIDTH = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN
+SCREEN_HEIGHT = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN
 SCREEN_TITLE = "JDE Project"
 
 MOVE_SPEED = 1
@@ -22,7 +28,15 @@ PLAYER_SIZE = 12
 
 class Player(arcade.Sprite):
 
-    def update(self):
+    def update(self, left_coll: bool, right_coll: bool, top_coll: bool, bottom_coll: bool):
+        if left_coll and self.change_x < 0:
+            pass
+        if right_coll and self.change_x > 0:
+            pass
+        if top_coll and self.change_y > 0:
+            pass
+        if bottom_coll and self.change_y < 0:
+            pass
         self.center_x += self.change_x
         self.center_y += self.change_y
 
@@ -30,20 +44,28 @@ class Player(arcade.Sprite):
         pass
 
 
-class SolidWall(arcade.SpriteSolidColor):
+# NOTE: for the wall sprites, if the row number of the array is odd, they will be rotated vertically. Otherwise,
+# they wont be rotated
 
-    def __init__(self, center_x, center_y):
-        super().__init__(TILE_SIZE, TILE_SIZE, arcade.color.DARK_GRAY)
+class Wall(arcade.Sprite):
+
+    def __init__(self, solid: bool, center_x: int, center_y: int, scale: float, vertical: bool = False):
+        if solid:
+            super().__init__('sprites/wall_00.png', scale=scale)
+        else:
+            super().__init__('sprites/log_00.png', scale=scale)
+
+        if vertical:
+            self.angle = random.choice([90, 270])
+        else:
+            self.angle = 0
+
         self.center_x = center_x
         self.center_y = center_y
 
-
-class WoodenWall(arcade.SpriteSolidColor):
-
-    def __init__(self, center_x, center_y):
-        super().__init__(TILE_SIZE, TILE_SIZE, arcade.color.WOOD_BROWN)
-        self.center_x = center_x
-        self.center_y = center_y
+    def update(self):
+        if self.angle >= 360:
+            self.angle -= 360
 
 
 class Game(arcade.Window):
@@ -61,65 +83,70 @@ class Game(arcade.Window):
         super().__init__(width, height, title)
 
         self.player1 = None
-        self.p1_pos = []
-        self.p1_x = None
-        self.p1_y = None
-        self.p1_move = []
 
         self.player2 = None
-        self.p2_pos = []
-        self.p2_x = None
-        self.p2_y = None
-        self.p2_move = []
 
         self.grid = None
 
         self.sprite_array = []
 
-        self.sprite_list = None
+        self.player_list = None
+        self.wooden_wall_list = None
+        self.solid_wall_list = None
 
         arcade.set_background_color(arcade.color.BLACK)
 
-    def setup(self, size: int = 27, center_size: int = 2):
+    def setup(self, size_x: int = COLUMN_COUNT, size_y: int = ROW_COUNT, center_size: int = 2):
         """
         setup the game
         :param size: the size of the grid (at least 27)
         :param center_size: the size of the center meetup area (at least 2)
         :return:
         """
-        self.p1_x = 1
-        self.p1_y = 1
-        self.player1 = Player('sprites/p1_32_0.png', scale=SCREEN_SCALE)
 
-        self.p2_x = size - 2
-        self.p2_y = size - 2
+        self.player1 = Player('sprites/p1_32_0.png', scale=SCREEN_SCALE)
         self.player2 = Player('sprites/p2_32_0.png', scale=SCREEN_SCALE)
 
-        self.grid = worldgrid.generate(size, center_size)
-        self.grid[self.p1_y][self.p1_x] = '1'
-        self.grid[self.p2_y][self.p2_x] = '2'
-        self.sprite_list = arcade.SpriteList()
+        self.grid = worldgrid.generate(COLUMN_COUNT, ROW_COUNT, center_size)
+        # self.grid[self.p1_y][self.p1_x] = '1'
+        # self.grid[self.p2_y][self.p2_x] = '2'
+
+        self.solid_wall_list = arcade.SpriteList()
+        self.wooden_wall_list = arcade.SpriteList()
+        self.player_list = arcade.SpriteList()
 
         for y in range(len(self.grid)):
             self.sprite_array.append([])
             for x in range(len(self.grid[y])):
-                if self.grid[y][x] in ['S', 'X']:
-                    self.sprite_array[y].append(SolidWall(x * TILE_SIZE + TILE_SIZE // 2, y * TILE_SIZE + TILE_SIZE // 2))
-                    self.sprite_list.append(self.sprite_array[y][x])
+                sprite_x = x * WALL_WIDTH + WALL_WIDTH // 2 + ((x - 1) * WALL_DEPTH)
+                sprite_y = y * WALL_WIDTH + WALL_WIDTH // 2 + ((y - 1) * WALL_DEPTH)
+                vertical = None
+                # if y is odd and x is even, vertical = True
+                if y % 2 == 1 and x % 2 == 0:
+                    vertical = True
+                # if y is odd and x is odd, do nothing
+                # if y is even and x is even, it's a dot-wall, not a thin wall
+                # if y is even and x is odd, vertical = False
+                elif y % 2 == 0 and x % 2 == 1:
+                    vertical = False
+                if self.grid[y][x] == 'S':
+                    self.solid_wall_list.append(Wall(True, sprite_x, sprite_y, SCREEN_SCALE, vertical))
                 elif self.grid[y][x] == 'W':
-                    self.sprite_array[y].append(WoodenWall(x * TILE_SIZE + TILE_SIZE // 2, y * TILE_SIZE + TILE_SIZE // 2))
-                    self.sprite_list.append(self.sprite_array[y][x])
+                    self.wooden_wall_list.append(Wall(False, sprite_x, sprite_y, SCREEN_SCALE, vertical))
                 elif self.grid[y][x] == '1':
-                    self.sprite_array[y].append(self.player1)
-                    self.sprite_list.append(self.player1)
+                    self.player1.center_x = sprite_x
+                    self.player1.center_y = sprite_y
+                    self.player1.angle += 180
+                    print(sprite_x, sprite_y)
                 elif self.grid[y][x] == '2':
-                    self.sprite_array[y].append(self.player2)
-                    self.sprite_list.append(self.player2)
+                    self.player2.center_x = sprite_x
+                    self.player2.center_y = sprite_y
+                    print(sprite_x, sprite_y)
                 else:
                     self.sprite_array[y].append(None)
 
-        self.sprite_list.append(self.player1)
-        self.sprite_list.append(self.player2)
+        self.player_list.append(self.player1)
+        self.player_list.append(self.player2)
 
     def on_update(self, delta_time: float):
         """
@@ -127,16 +154,34 @@ class Game(arcade.Window):
         :param delta_time:
         :return:
         """
-        self.player1.center_x = self.p1_x * TILE_SIZE + TILE_SIZE // 2
-        self.player1.center_y = self.p1_y * TILE_SIZE + TILE_SIZE // 2
+        # self.player1.center_x = self.p1_x * TILE_SIZE + TILE_SIZE // 2
+        # self.player1.center_y = self.p1_y * TILE_SIZE + TILE_SIZE // 2
 
-        self.player1.update()
+        left_coll = False
+        right_coll = False
+        top_coll = False
+        bottom_coll = False
+        wooden_wall_hit_list1 = arcade.check_for_collision_with_list(self.player1, self.wooden_wall_list)
+        for wall in wooden_wall_hit_list1:
+            if wall.top <= self.player1.top < wall.bottom:
+                print('wood top coll')
+                top_coll = True
+            if wall.bottom <= self.player1.bottom < wall.top:
+                print('wood bottom coll')
+                bottom_coll = True
+            if wall.left <= self.player1.left < wall.right:
+                print('wood left coll')
+                left_coll = True
+            if wall.right >= self.player1.right > wall.left:
+                print('wood right coll')
+                right_coll = True
+        solid_wall_hit_list1 = arcade.check_for_collision_with_list(self.player1, self.solid_wall_list)
+        self.player1.update(left_coll, right_coll, top_coll, bottom_coll)
 
-        self.player2.center_x = self.p2_x * TILE_SIZE + TILE_SIZE // 2
-        self.player2.center_y = self.p2_y * TILE_SIZE + TILE_SIZE // 2
+        # self.player2.center_x = self.p2_x * TILE_SIZE + TILE_SIZE // 2
+        # self.player2.center_y = self.p2_y * TILE_SIZE + TILE_SIZE // 2
 
-        self.player2.update()
-
+        # self.player2.update()
     def on_draw(self):
         """
         update the screen
@@ -144,11 +189,9 @@ class Game(arcade.Window):
         """
         arcade.start_render()
 
-        # draw the objects in the shape list
-        self.sprite_list.draw()
-
-        # draw the sprites in the sprite list
-        self.sprite_list.draw()
+        self.solid_wall_list.draw()
+        self.wooden_wall_list.draw()
+        self.player_list.draw()
 
     def on_key_press(self, symbol: int, modifiers: int):
         """
@@ -159,47 +202,43 @@ class Game(arcade.Window):
         """
         # process input for player 1
         if symbol == arcade.key.W:
-            pass  # move up
-            print('w')
+            self.player1.change_y = MOVE_SPEED  # move up
+            # print('w')
         if symbol == arcade.key.A:
-            pass  # move left
-            print('a')
+            self.player1.change_x = -MOVE_SPEED  # move left
+            # print('a')
         if symbol == arcade.key.S:
-            pass  # move down
-            print('s')
+            self.player1.change_y = -MOVE_SPEED  # move down
+            # print('s')
         if symbol == arcade.key.D:
-            pass  # move right
-            print('d')
+            self.player1.change_x = MOVE_SPEED  # move right
+            # print('d')
 
         # process input for player 2
-        if symbol in [arcade.key.UP, arcade.key.LEFT, arcade.key.DOWN, arcade.key.RIGHT]:
-            self.grid[self.p2_y][self.p2_x] = ' '
-            if symbol == arcade.key.UP:
-                pass  # move up
-            if symbol == arcade.key.LEFT:
-                pass  # move left
-            if symbol == arcade.key.DOWN:
-                pass  # move down
-            if symbol == arcade.key.RIGHT:
-                pass  # move right
-            self.grid[self.p2_y][self.p2_x] = '2'
+        if symbol == arcade.key.UP:
+            pass  # move up
+        if symbol == arcade.key.LEFT:
+            pass  # move left
+        if symbol == arcade.key.DOWN:
+            pass  # move down
+        if symbol == arcade.key.RIGHT:
+            pass  # move right
 
         if symbol == arcade.key.RSHIFT:
             pass  # spin player 1's buzzsaw
 
-        if symbol == arcade.key.LSHIFT:  # spin player 2's buzzsaw
-            if symbol == arcade.key.UP and self.grid[self.p2_y-1][self.p2_x] == 'W':
-                pass
-            if symbol == arcade.key.LEFT and self.grid[self.p2_y][self.p2_x-1] == 'W':
-                pass
-            if symbol == arcade.key.DOWN and self.grid[self.p2_y+1][self.p2_x] == 'W':
-                pass
-            if symbol == arcade.key.RIGHT and self.grod[self.p2_y][self.p2_x+1] == 'W':
-                pass
-
+        if symbol == arcade.key.LSHIFT:
+            pass  # spin player 2's buzzsaw
 
     def on_key_release(self, symbol: int, modifiers: int):
-        pass
+
+        # reset the movement deltas for player 1
+        if symbol in [arcade.key.W, arcade.key.S]:
+            # print('release y')
+            self.player1.change_y = 0
+        if symbol in [arcade.key.A, arcade.key.D]:
+            # print('release x')
+            self.player1.change_x = 0
 
 
 def main():
--- worldgrid.py
+++ worldgrid.py
@@ -1,14 +1,20 @@
 import random
 
+MIN_GRID_SIZE = 11
 
-def generate(size: int = 27, center_size: int = 2):
+def generate(size_x: int = 17, size_y: int = 17, center_size: int = 2):
     grid = []
 
     # make sure the size is at least 27 and is an odd number
-    if size <= 27:
-        size = 27
-    elif size % 2 == 0:
-        size += 1
+    if size_x <= MIN_GRID_SIZE:
+        size_x = MIN_GRID_SIZE
+    elif size_x % 2 == 0:
+        size_x += 1
+
+    if size_y <= MIN_GRID_SIZE:
+        size_y = MIN_GRID_SIZE
+    elif size_y % 2 == 0:
+        size_y += 1
 
     # make sure the center_size is at least 2 and an even number
     if center_size < 2:
@@ -17,19 +23,25 @@ def generate(size: int = 27, center_size: int = 2):
         center_size += 1
 
     # create an easy reference variable for the range representing the area of the center
-    mid_range = range(size//2-center_size, size//2+center_size+1)
+    mid_range_x = range(size_x//2-center_size, size_x//2+center_size+1)
+    mid_range_y = range(size_y//2-center_size, size_y//2+center_size+1)
 
-    for y in range(size):
+    for y in range(size_y):
         grid.append([])
-        for x in range(size):
-            if x == 0 or y == 0 or x == size - 1 or y == size - 1:
+        for x in range(size_x):
+            if x == 0 or y == 0 or x == size_x - 1 or y == size_y - 1 or (x % 2 == 0 and y % 2 == 0):
                 grid[y].append('X')
-            elif x in mid_range and y in mid_range:
-                grid[y].append(' ')
-            elif x % 2 == 0 and y % 2 == 0:
-                grid[y].append('S')
+            elif x in mid_range_x and y in mid_range_y:
+                if (x == mid_range_x.start or x == mid_range_x.stop-1) or (y == mid_range_y.start or y == mid_range_y.stop-1):
+                    grid[y].append('W')
+                else:
+                    grid[y].append(' ')
             elif x % 2 == 0 or y % 2 == 0:
-                grid[y].append(random.choice(['W', 'W', 'S']))
+                grid[y].append(random.choice(['W', 'S']))
+            elif x == 1 and y == 1:
+                grid[y].append('1')
+            elif x == size_x-2 and y == size_y-2:
+                grid[y].append('2')
             else:
                 grid[y].append(' ')
 
@@ -37,3 +49,5 @@ def generate(size: int = 27, center_size: int = 2):
         print()
 
     return grid
+
+# generate()
